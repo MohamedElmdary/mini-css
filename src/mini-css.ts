0631894ce66ad957f36f0996773046df93ec3d68
MohamedElmdary <engm5081@gmail.com>
2019-07-06 03:51:48 +0200

    added split method

--- src/mini-css.ts
+++ src/mini-css.ts
@@ -1,27 +1,25 @@
 class MiniCss {
-  private _tokens: [] = [];
+  private $$css: string;
 
-  constructor(private css: string) {}
+  constructor(css: string) {
+    this.$$css = css;
+  }
 
   public static compile(css: string): string {
-    const miniCss: MiniCss = new MiniCss(css);
-    miniCss;
+    const mc = new MiniCss(css);
     return "";
   }
 
-  get tokens() {
-    return this._tokens;
+  private split(): string[] {
+    const blocks: string[] = [];
+    let current = 0;
+    for (;;) {
+      const n = this.$$css.indexOf("}", current);
+      if (n === -1) break;
+      const block = this.$$css.slice(current, n + 1);
+      blocks.push(block);
+      current = n + 1;
+    }
+    return blocks;
   }
-
-  /*
-  public split() {}
-
-  public tokenizer() {}
-
-  public combine() {}
-
-  public minify() {}
-
-  public generateCss() {}
-*/
 }
