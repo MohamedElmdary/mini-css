0c89d539a6fcfb75b6382087c6dbf5b60123d826
MohamedElmdary <engm5081@gmail.com>
2019-07-06 15:23:04 +0200

    added mocha & expect for test

---
+++ test/mini-css.spec.ts
@@ -0,0 +1,9 @@
+import expect from "expect";
+
+describe("Mini Css Tests", () => {
+  describe("main functions", () => {
+    it("should be equal", () => {
+      expect(1).toEqual(1);
+    });
+  });
+});
